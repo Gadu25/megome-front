bb14735f4cf4659aacdafbaf4dbade8912ef6a0e
Gadu25 <alexanderudag@gmail.com>
2026-03-16 13:46:12 +0800

    feat: add logo component

---
+++ components/common/logo.tsx
@@ -0,0 +1,15 @@
+export function LogoFull() {
+  return (
+    <div className="font-bold text-xl lg:text-3xl tracking-widest">
+      megome
+    </div>
+  )
+}
+
+export function LogoSmall() {
+  return (
+    <div className="font-bold text-xl tracking-widest">
+      m
+    </div>
+  )
+}
