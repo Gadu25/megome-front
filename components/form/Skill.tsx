ef98be1d07e5f2244713feebb5c90cb3370a4973
Gadu25 <alexanderudag@gmail.com>
2026-04-23 06:20:14 +0800

    feat: add modal confirmation on logout and skill delete

--- components/form/Skill.tsx
+++ components/form/Skill.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react'
 import { skillApi } from '@/lib/api/skillApi'
 import { Skill } from '@/types/types'
+import Modal from '../modal/Modal'
+import  { XMarkIcon } from '@heroicons/react/24/outline'
 
 const PROFICIENCY_OPTIONS: Skill['proficiency'][] = [
   'Beginner',
@@ -17,10 +19,9 @@ type Props = {
 export default function ProfileSkillForm({ initialSkills, setSkills }: Props) {
   const { addSkill, updateSkill, deleteSkill } = skillApi();
   const debounceRef = useRef<Record<number, NodeJS.Timeout>>({});
-  const [newSkill, setNewSkill] = useState({
-    skillName: '',
-    proficiency: 'Beginner' as Skill['proficiency'],
-  })
+  const [newSkill, setNewSkill] = useState({ skillName: '', proficiency: 'Beginner' as Skill['proficiency'] })
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedSkillId, setSelectedSkillId] = useState<number | null>(null);
 
   const handleUpdateSkill = (id: number, field: keyof Skill, value: Skill[keyof Skill] ) => {
     let updatedSkill: Skill | undefined;
@@ -64,29 +65,63 @@ export default function ProfileSkillForm({ initialSkills, setSkills }: Props) {
     })
   }
 
-  const handleDeleteSkill = async (id: number) => {
-    const res = await deleteSkill(id);
+  const handleDeleteSkill = async () => {
+    if (selectedSkillId === null) return;
+    const res = await deleteSkill(selectedSkillId);
+    setSelectedSkillId(null);
     setSkills(res.data.skills);
   }
 
+  const proceedCancel = async () => {
+    handleDeleteSkill()
+  }
+
   return (
-    <div className="space-y-6">
-      <div className="space-y-4">
-        {initialSkills.map((skill) => (
-          <div key={skill.id} className="flex gap-2 items-center">
-            <input
-              type="text"
-              className="input input-bordered w-full"
-              value={skill.skillName}
-              onChange={(e) =>
-                handleUpdateSkill(skill.id, 'skillName', e.target.value)
-              }
+    <>
+      <div className="space-y-6">
+        <div className="space-y-4">
+          {initialSkills.map((skill) => (
+            <div key={skill.id} className="flex gap-2 items-center">
+              <input type="text" className="input input-bordered w-full" value={skill.skillName}
+                onChange={(e) =>
+                  handleUpdateSkill(skill.id, 'skillName', e.target.value)
+                }
+              />
+
+              <select
+                className="select select-bordered"
+                value={skill.proficiency}
+                onChange={(e) => handleUpdateSkill(skill.id, 'proficiency', e.target.value as Skill['proficiency'])}
+              >
+                {PROFICIENCY_OPTIONS.map((level) => (
+                  <option key={level} value={level}>
+                    {level}
+                  </option>
+                ))}
+              </select>
+
+              <button className="btn btn-error btn-xs"
+                onClick={() => {
+                  setSelectedSkillId(skill.id);
+                  setModalOpen(true);
+                }}
+              >
+                <XMarkIcon className="size-4"/>
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <div className="border-t pt-4 space-y-2">
+          <h3 className="font-medium text-sm opacity-70">Add Skill</h3>
+
+          <div className="flex gap-2">
+            <input type="text" placeholder="Skill name" className="input input-bordered w-full" value={newSkill.skillName}
+              onChange={(e) => setNewSkill((prev) => ({ ...prev, skillName: e.target.value, }))}
             />
 
-            <select
-              className="select select-bordered"
-              value={skill.proficiency}
-              onChange={(e) => handleUpdateSkill(skill.id, 'proficiency', e.target.value as Skill['proficiency'])}
+            <select className="select select-bordered" value={newSkill.proficiency}
+              onChange={(e) => setNewSkill((prev) => ({ ...prev, proficiency: e.target.value as Skill['proficiency'], }))}
             >
               {PROFICIENCY_OPTIONS.map((level) => (
                 <option key={level} value={level}>
@@ -95,45 +130,22 @@ export default function ProfileSkillForm({ initialSkills, setSkills }: Props) {
               ))}
             </select>
 
-            <button
-              className="btn btn-error btn-sm"
-              onClick={() => handleDeleteSkill(skill.id)}
-            >
-              ✕
+            <button className="btn btn-primary" onClick={handleAddSkill}>
+              Add
             </button>
           </div>
-        ))}
-      </div>
-
-      <div className="border-t pt-4 space-y-2">
-        <h3 className="font-medium text-sm opacity-70">Add Skill</h3>
-
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Skill name"
-            className="input input-bordered w-full"
-            value={newSkill.skillName}
-            onChange={(e) => setNewSkill((prev) => ({ ...prev, skillName: e.target.value, }))}
-          />
-
-          <select
-            className="select select-bordered"
-            value={newSkill.proficiency}
-            onChange={(e) => setNewSkill((prev) => ({ ...prev, proficiency: e.target.value as Skill['proficiency'], }))}
-          >
-            {PROFICIENCY_OPTIONS.map((level) => (
-              <option key={level} value={level}>
-                {level}
-              </option>
-            ))}
-          </select>
-
-          <button className="btn btn-primary" onClick={handleAddSkill}>
-            Add
-          </button>
         </div>
       </div>
-    </div>
+      <Modal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        title="Delete Skill"
+        onAccept={() => proceedCancel()}
+        onCancel={() => setModalOpen(false)}
+        acceptText="Delete"
+      >
+        <p>Are you sure you want to delete this item? This is irreversible.</p>
+      </Modal>
+    </>
   )
 }
