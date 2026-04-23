4ac9c97ac40c61c85d71fc315f4e2bf7624c492d
Gadu25 <alexanderudag@gmail.com>
2026-04-24 06:38:54 +0800

    fix: update arrangement of right content

--- components/profile/RightContent.tsx
+++ components/profile/RightContent.tsx
@@ -5,18 +5,20 @@ import ProfileProjects from "./ProfileProjects";
 import ProfileEducation from "./ProfileEducation";
 import ProfileExperience from "./ProfileExperience";
 
+type Tab = 'projects' | 'experience' | 'education';
+
 export default function RightContent() {
-  const [activeTab, setActiveTab] = useState<'projects' | 'experience' | 'education'>('projects');
+  const [activeTab, setActiveTab] = useState<Tab>('education');
   return (
     <>
     <div className="space-y-4 lg:col-span-2">
 
 
       <div role="tablist" className="tabs tabs-boxed">
-        <button role="tab" className={`tab ${activeTab === 'projects' ? 'tab-active' : ''}`}
-          onClick={() => setActiveTab('projects')}
+        <button role="tab" className={`tab ${activeTab === 'education' ? 'tab-active' : ''}`}
+          onClick={() => setActiveTab('education')}
         >
-          Projects
+          Education
         </button>
 
         <button role="tab" className={`tab ${activeTab === 'experience' ? 'tab-active' : ''}`}
@@ -25,15 +27,14 @@ export default function RightContent() {
           Experience
         </button>
 
-        <button role="tab" className={`tab ${activeTab === 'education' ? 'tab-active' : ''}`}
-          onClick={() => setActiveTab('education')}
+        <button role="tab" className={`tab ${activeTab === 'projects' ? 'tab-active' : ''}`}
+          onClick={() => setActiveTab('projects')}
         >
-          Education
+          Projects
         </button>
       </div>
 
       <div className="w-full">
-
         {activeTab === 'projects' && (
           <ProfileProjects />
         )}
