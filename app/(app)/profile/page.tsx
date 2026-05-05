36d57bdaff37089f5f4d066ec88d937cf4495179
Gadu25 <alexanderudag@gmail.com>
2026-05-05 13:06:47 +0800

    refactor: remove unecessary comments and lines

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -6,34 +6,14 @@ export default function ProfilePage() {
   
   return (
     <div className="mx-auto space-y-6">
-
-      {/* TOP PROFILE */}
       <div className="card bg-base-100 shadow p-6 flex flex-col lg:flex-row gap-6">
         <TopProfile/>
       </div>
-
-      {/* MAIN CONTENT */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-
-        {/* LEFT SIDEBAR */}
         <div className="space-y-6">
-
           <ProfileSkill/>
-
-          {/* API Status */}
-          {/* <div className="card bg-base-100 shadow p-5">
-            <h2 className="font-semibold mb-2">API Status</h2>
-            <div className="badge badge-success">Connected</div>
-            <p className="text-sm text-base-content/60 mt-2">
-              Your portfolio is live via API.
-            </p>
-          </div> */}
-
         </div>
-
-        {/* RIGHT CONTENT */}
         <RightContent/>
-
       </div>
     </div>
   );
