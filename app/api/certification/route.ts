ce8a15284f3c4f11e6658267d6349cf8d944efc7
Gadu25 <alexanderudag@gmail.com>
2026-05-15 09:46:38 +0800

    refactor: apply BFF setup for certificate api

---
+++ app/api/certification/route.ts
@@ -0,0 +1,72 @@
+import { NextResponse } from "next/server";
+import { getAccessToken } from "@/lib/auth/cookies";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL!;
+
+export async function GET() {
+  try {
+    const accesToken = await getAccessToken();
+
+    const response = await fetch(
+      `${BACKEND_URL}/api/v1/certification`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accesToken}`,
+        }
+      }
+    )
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return NextResponse.json(
+        {
+          message: "Failed to fetch certificates",
+          certificates: [],
+        }
+      )
+    }
+
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json(
+       {
+        message: err,
+        certificates: [],
+       }
+    )
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const accessToken = await getAccessToken();
+    const body = await req.json();
+
+    const response = await fetch(`${BACKEND_URL}/api/v1/certification`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return NextResponse.json(
+        { message: data.error || "Failed to add certificate" },
+        { status: response.status }
+      );
+    }
+
+    return NextResponse.json(data);
+  } catch (_) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
