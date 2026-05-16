5b68b61f32fb374e039c1b6a31be742bc2698707
Gadu25 <alexanderudag@gmail.com>
2026-05-16 10:17:09 +0800

    refactor: migrate projects API caller to BFF

--- lib/api/client/project.ts
+++ lib/api/client/project.ts
@@ -12,6 +12,11 @@ interface SingleResponse {
   project: ProjectFull;
 }
 
+interface SingleImageResponse {
+  message: string;
+  image: ProjectImage
+}
+
 export const getProjectClient = async (id: number) => {
   const res = await fetchClient(
     `/api/project/${id}`,
@@ -40,13 +45,29 @@ export const addProjectClient = async (form: ProjectForm) => {
   const res = await fetchClient(
     "/api/project",
     {
-      method: "PUT",
+      method: "POST",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(form)
     }
+  )
+
+  return handleResponse<SingleResponse>(res);
+}
+
+export const updateProjectClient = async (id: number, form: ProjectForm) => {
+  const res = await fetchClient(
+    `/api/project/${id}`,
+    {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    },
   )
 
   return handleResponse<SingleResponse>(res);
@@ -62,4 +83,40 @@ export const deleteProjectClient = async (id: number) => {
   )
 
   return handleResponse<SingleResponse>(res);
+}
+
+export const uploadProjectImageClient = async (id: number, projectImage : ProjectImage) => {
+  const formData = new FormData();
+
+  if (projectImage.file) {
+    formData.append("image", projectImage.file);
+    console.log("file", projectImage.file)
+  }
+  formData.append("type", projectImage.type);
+
+  const res = await fetchClient(`/api/project/${id}/images`, {
+    method: "POST",
+    credentials: "include",
+    body: formData,
+  })
+
+  return handleResponse<SingleImageResponse>(res);
+}
+
+export const uploadCoverImageClient = async (id: number, projectCover: ProjectImage) => {
+  const formData = new FormData();
+
+  if (projectCover.file) {
+    formData.append("image", projectCover.file);
+    console.log("file", projectCover.file)
+  }
+  formData.append("type", projectCover.type);
+
+  const res = await fetchClient(`/api/project/${id}/cover`, {
+    method: "PUT",
+    credentials: "include",
+    body: formData,
+  })
+
+  return handleResponse<SingleImageResponse>(res);
 }
