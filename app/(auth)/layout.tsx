018f250de8c79bb77e719407d8130ece24077399
Gadu25 <alexanderudag@gmail.com>
2026-06-10 14:10:29 +0800

    fix: resolve issue on using useSearchParams

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { Suspense } from "react";
+
+export default function AuthLayout({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <Suspense>
+      {children}
+    </Suspense>
+  );
+}
